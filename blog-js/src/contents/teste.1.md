980ca0701cc40c189318f9a47198452a0eaf6a70
Deivid Veloso <veloso.deivid@gmail.com>
2018-06-20 11:43:43 -0300

    clean

--- blog-js/src/contents/teste.1.md
+++
@@ -1,7 +0,0 @@
----
-path: "/blog/my-first-post2"
-date: "2017-11-07"
-title: "My first blog post 2"
----
-
-Teste Markdown 3
---
+++ tutorial-part-one/src/pages/page-3.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () => (
+  <div>
+    <p>Hello world from my second Gatsby page 333</p>
+    <Link to="/">back home</Link>
+  </div>
+);
